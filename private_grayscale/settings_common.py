340a6777bf824743b65df3133ffa38d826746f2d
hoshinyonn <ngn2149610@stu.o-hara.ac.jp>
2022-07-28 14:00:26 +0900

    グループワーク２

--- private_grayscale/settings_common.py
+++ private_grayscale/settings_common.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'grayscale',
-        'USER':'postgres',
-        'PASSWORD':'naganose',
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASSWORD'),
         'HOST':'',
         'PORT':'',
     }
